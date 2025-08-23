25b1a0b18df57bbf16b867411bda2dc8f82cc624
Admin <saisudha9192@gmail.com>
2025-08-23 16:17:09 +0530

    exploring HTTP methos - GET, POST, PATCH, PUT, DELETE

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 const express = require("express");
 
 const app = express();
-app.use('/',(req,res)=>{
-    res.send('Namasthe Akshay');
+
+// HTTP methods
+
+/* GET call */
+app.get('/user',(req,res) => {
+    res.send({'firstName':'Sai Sudha','lastName':'Mudigonda'});
 });
+
+//POST call
+app.post('/user',(req,res) => {
+    //save into DB
+    res.send('User data got saved successfully!');
+});
+
+/*PUT call */
+app.put('/user',(req,res) => {
+    // update into DB
+    res.send('User data got updated successfully');
+});
+
+/* PATCH call */
+app.patch('/user',(req,res) => {
+    // new record into DB
+    res.send('New record got created for existing user');
+});
+
+/* Delete call */
+app.delete('/user',(req,res) => {
+    // delete record from DB
+    res.send('Deleted successfully !');
+});
+
+
+
 
 app.use('/learn',(req,res) => { 
     res.send('This is initial learnining of node');
@@ -14,6 +45,9 @@ app.use('/test',(req,res)=>{
 
 app.use('/hello',(req,res) =>{
     res.send('Hello Hello Helllo');
+});
+app.use('/',(req,res)=>{
+    res.send('Namasthe Akshay');
 });
 
 
