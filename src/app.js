e1b6d13d258ac40fb5541b3aa1505ddf8df88b90
Admin <saisudha9192@gmail.com>
2025-08-25 12:32:58 +0530

    played with query params & dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+
+// query params and dynamic routes
+/* query params */
+app.get('/user/:userId/:pwd', (req,res) => {
+    console.log(req.params);
+    res.send('Saved user info into DB');
+})
+
 // HTTP methods
 
 /* GET call */
@@ -9,13 +17,13 @@ app.get('/user',(req,res) => {
     res.send({'firstName':'Sai Sudha','lastName':'Mudigonda'});
 });
 
-//POST call
+/* POST call */
 app.post('/user',(req,res) => {
     //save into DB
     res.send('User data got saved successfully!');
 });
 
-/*PUT call */
+/* PUT call */
 app.put('/user',(req,res) => {
     // update into DB
     res.send('User data got updated successfully');
@@ -32,10 +40,9 @@ app.delete('/user',(req,res) => {
     // delete record from DB
     res.send('Deleted successfully !');
 });
+// END of HTTP methods
 
-
-
-
+// .use method will takeouver all routing methods.
 app.use('/learn',(req,res) => { 
     res.send('This is initial learnining of node');
 });
@@ -49,6 +56,8 @@ app.use('/hello',(req,res) =>{
 app.use('/',(req,res)=>{
     res.send('Namasthe Akshay');
 });
+
+// END of .use method
 
 
 
